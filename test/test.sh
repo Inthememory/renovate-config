1075529999b4514a8a662ed77dfedf13f9eca5f6
Julien Polo <julien.polo@gmail.com>
2026-03-24 15:48:10 +0100

    chore(test): switch from npm to pnpm for running renovate-config-validator

--- test/test.sh
+++ test/test.sh
@@ -4,5 +4,5 @@ set -e
 preset_files=$(grep -lri --exclude-dir=node_modules --include="*.json" "https://docs.renovatebot.com/renovate-schema.json" .)
 
 for preset_file in $preset_files; do
-  RENOVATE_CONFIG_FILE="$preset_file" npm exec -- renovate-config-validator
+  RENOVATE_CONFIG_FILE="$preset_file" pnpm exec -- renovate-config-validator
 done
