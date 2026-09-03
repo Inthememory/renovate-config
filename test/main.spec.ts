bd756c6d1d504123898508932d478178e8514b2a
Julien Polo <julien.polo@vusion.com>
2026-09-03 11:19:09 +0200

    test(config): update snapshots and tests for default.json entrypoint

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,17 +1,13 @@
 import { describe, expect, test } from 'vitest';
 
 import baseJSON from '../_base.json';
-import applicationJSON from '../application.json';
-import libraryJSON from '../library.json';
+import defaultJSON from '../default.json';
 
 describe('Configuration snapshot', () => {
   test('_base.json', () => {
     expect(baseJSON).toMatchSnapshot();
   });
-  test('application.json', () => {
-    expect(applicationJSON).toMatchSnapshot();
-  });
   test('library.json', () => {
-    expect(libraryJSON).toMatchSnapshot();
+    expect(defaultJSON).toMatchSnapshot();
   });
 });
